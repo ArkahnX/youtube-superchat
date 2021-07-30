e10946d57c24c45704009b7f6eb3704bd826ef30
Christopher Hall <1167959+ArkahnX@users.noreply.github.com>
2021-07-29 19:25:58 -0700

    functional

--- build/main.js
+++ build/main.js
@@ -2,134 +2,178 @@
 "use strict";
 
 const limit = [350, 330, 310, 290, 270, 250, 225, 200, 150, 50];
-limit.reverse();
-const price = [500, 400, 300, 200, 100, 50, 20, 10, 5, 2];
-price.reverse();
+const prices = [500, 400, 300, 200, 100, 50, 20, 10, 5, 2];
 
 // https://stackoverflow.com/questions/53659151/return-all-subsets-whose-sum-is-a-given-value-subset-sum-problem
 
-const textInput = document.createElement("textarea");
-const textData = document.createElement("pre");
+const textInput = document.createElement("span");
+textInput.setAttribute("class", "textarea");
+textInput.setAttribute("maxlength", "1000");
+textInput.setAttribute("contenteditable", true);
+textInput.innerText = "This is a sample superchat message! Lorem Ipsum... This will appear in a new $2 superchat.";
+const textData = document.createElement("div");
+const superChatValues = document.getElementById("superchatvalues");
+const superchattextbox = document.getElementById("superchattextbox");
 textInput.addEventListener("keyup", calculate);
-document.getElementById("main").appendChild(textInput);
+
+// superChatSelect.addEventListener("change", calculate);
+for (const [key, value] of prices.entries()) {
+	const optionElement = document.createElement("input");
+	const superchatElement = document.createElement("div");
+	const spanElement = document.createElement("span");
+	const labelElement = document.createElement("label");
+	const inputElement = document.createElement("input");
+	labelElement.innerText = `$${value}`;
+	spanElement.appendChild(labelElement);
+	labelElement.appendChild(inputElement);
+	inputElement.setAttribute("id", `sc_${key}`);
+	superchatElement.setAttribute("data-superchat-id", key);
+	superchatElement.setAttribute("class", `${superchatTier(key)} superchat-card`);
+	spanElement.setAttribute("class", `contents`);
+	inputElement.setAttribute("value", value);
+	inputElement.setAttribute("type", "text");
+	optionElement.setAttribute("value", key);
+	optionElement.setAttribute("checked", true);
+	optionElement.setAttribute("id", `sco_${key}`);
+	optionElement.setAttribute("type", "checkbox");
+	optionElement.addEventListener("change", calculate);
+	spanElement.appendChild(optionElement);
+	superchatElement.appendChild(spanElement);
+	superChatValues.appendChild(superchatElement);
+}
+
+superchattextbox.appendChild(textInput);
 document.getElementById("main").appendChild(textData);
 let timer = -1;
-function calculate(event) {
+
+function toggleSuperchatColors() {
+	for (const element of superChatValues.children) {
+		// console.log(
+		// 	element,
+		// 	element.children[0].children[1],
+		// 	superchatTier(element.dataset.superchatId),
+		// 	element.dataset.superchatId,
+		// );
+		if (element.children[0].children[1].checked === false) {
+			element.classList.remove(superchatTier(parseInt(element.dataset.superchatId, 10)));
+			element.classList.add("none");
+		} else {
+			element.classList.add(superchatTier(parseInt(element.dataset.superchatId, 10)));
+			element.classList.remove("none");
+		}
+	}
+}
+
+function calculate() {
 	clearTimeout(timer);
+	toggleSuperchatColors();
 	timer = setTimeout(function () {
-		const textLength = textInput.value.length;
-		const options = getSubsets(limit, textLength);
-		textData.innerText = textLength + "\n" + JSON.stringify(options, null, "\t");
+		const values = getSelectedSuperchatOptions();
+		const textLength = textInput.innerText.length;
+		// console.log(textLength);
+		const options = getSubsets(values, textLength);
+		const fragment = document.createDocumentFragment();
+		for (const option of options) {
+			let remainingText = textInput.innerText;
+			let price = 0;
+			fragment.appendChild(document.createElement("hr"));
+			const superchatElement = document.createElement("div");
+			const superchatHeaderElement = document.createElement("div");
+			const superChatStatsElement = document.createElement("div");
+			superchatElement.setAttribute("class", `none superchat-card`);
+			superchatHeaderElement.setAttribute("class", `header`);
+			superChatStatsElement.setAttribute("class", `contents`);
+			superchatHeaderElement.innerText = `Stats`;
+			superchatElement.appendChild(superchatHeaderElement);
+			superchatElement.appendChild(superChatStatsElement);
+			fragment.appendChild(superchatElement);
+			for (const index of option) {
+				const characterLimit = limit[index];
+				const dollarValue = document.getElementById(`sc_${index}`).value;
+				const message = remainingText.match(new RegExp(`(.|[\r\n]){1,${characterLimit}}`, "g"));
+				const superchatElement = document.createElement("div");
+				const superchatHeaderElement = document.createElement("div");
+				const preElement = document.createElement("div");
+				superchatElement.setAttribute("class", `${superchatTier(index)} superchat-card`);
+				superchatHeaderElement.setAttribute("class", `header`);
+				preElement.setAttribute("class", `contents`);
+				superchatHeaderElement.innerText = `$${dollarValue} \t (${message[0].length}/${limit[index]})`;
+				preElement.innerText = message[0];
+				superchatElement.appendChild(superchatHeaderElement);
+				superchatElement.appendChild(preElement);
+				fragment.appendChild(superchatElement);
+				message[0] = "";
+				remainingText = message.join("");
+				price += prices[index];
+			}
+			superChatStatsElement.innerText = `Cost: ${price}, Characters: ${textInput.innerText.length}`;
+		}
+		textData.innerText = "";
+		textData.appendChild(fragment);
+		// textData.innerText = textLength + "\n" + JSON.stringify(options, null, "\t");
 	}, 300);
 }
-// function findOptions(length, index = 0) {
-// 	const results = [];
-// 	for (index; index < limit.length; index++) {
-// 		let currentLength = length;
-// 		const result = {
-// 			limits: [],
-// 			quantity: 0,
-// 			price: 0,
-// 		};
-// 		console.log(limit[index], length);
-// 		if (limit[index] < length) {
-// 			result.limits.push(limit[index]);
-// 			currentLength = currentLength - limit[index];
-// 			let localIndex = index;
-// 			for (localIndex; localIndex < limit.length; localIndex++) {
-// 				if (currentLength > 0) {
-// 					currentLength = recursive(result, localIndex, currentLength);
-// 				}
-// 			}
-// 		} else {
-// 			result.limits.push(limit[index]);
-// 			currentLength = currentLength - limit[index];
-// 		}
-// 		console.log(result);
-// 		results.push(result);
-// 	}
-// 	return results;
-// }
 
-// function findOption(length) {
-// 	// const fit = [];
-// 	const options = {};
-// 	for (const item of limit) {
-// 		const option = [item];
-// 		const recursiveOption = [];
-// 		// fit.push(Math.ceil(length / item));
-// 		if (Math.ceil(length / item) > 1) {
-// 			// console.log(item, limit, recursiveOption);
-// 			recursive(length - item, recursiveOption);
-// 		}
-// 		console.log(item, option, recursiveOption);
-// 		console.log(JSON.stringify(options, null, "\t"));
-// 		// options[item] = [].concat(option, recursiveOption);
-// 	}
-// 	return options;
-// }
+function superchatTier(index) {
+	if (index < 5) {
+		return "aka";
+	}
+	if (index === 5) {
+		return "fifty";
+	}
+	if (index === 6) {
+		return "twenty";
+	}
+	if (index === 7) {
+		return "ten";
+	}
+	if (index === 8) {
+		return "five";
+	}
+	if (index === 9) {
+		return "two";
+	}
+}
 
-// function recursive(length, array) {
-// 	console.log(length, array);
-// 	for (const [key, value] of limit.entries()) {
-// 		const option = [value];
-// 		// fit.push(Math.ceil(length / item));
-// 		if (length > 0 && Math.ceil(length / value) > 1) {
-// 			option.push(value);
-// 			recursive(length - value, option);
-// 		} else {
-// 			// console.log(option);
-// 			option.push(value);
-// 		}
-// 		// console.log(length, item, option);
-// 		// console.log(array);
-// 		array.push(option);
-// 	}
-// }
+function getSelectedSuperchatOptions() {
+	const values = [];
+	for (const [key] of prices.entries()) {
+		if (document.getElementById(`sco_${key}`).checked) {
+			values.push(limit[key]);
+		}
+	}
+	// console.log(values);
+	return values;
+}
 
-// function count(textLength, containerLength) {}
-
-// function recursive(option, index, length) {
-// 	let currentLength = length;
-// 	console.log(index, length, limit[index]);
-// 	if (limit[index] < length) {
-// 		option.limits.push(limit[index]);
-// 		currentLength = currentLength - limit[index];
-// 		let localIndex = index;
-// 		for (localIndex; localIndex < limit.length; localIndex++) {
-// 			if (currentLength > 0) {
-// 				currentLength = recursive(option, localIndex, currentLength);
-// 			}
-// 		}
-// 	} else {
-// 		option.limits.push(limit[index]);
-// 		currentLength = currentLength - limit[index];
-// 	}
-// 	return currentLength;
-// }
-// console.log(findOption(500));
-//# sourceMappingURL=main.js.map
+// add termination condition, if s > 0?
 
 function getSubsets(array, sum) {
 	function fork(index = 0, s = 0, temp = []) {
-		if (s === sum) {
-			console.log(1, s, sum);
+		if (s >= sum) {
+			// console.log(1, s, sum);
 			result.push(temp);
+			// console.log("1");
+			// fork(index + 1, s, temp);
 			return;
 		}
 		if (index === array.length) {
-			console.log(2, index, array.length);
+			// console.log(2, index, array.length);
+			// console.log("2");
 			return;
 		}
 		if (s + array[index] <= sum) {
 			// shout circuit for positive numbers only
-			console.log(3, s, array[index], sum);
+			// console.log(3, s, array[index], sum);
+			// console.log("3");
 			// fork(index + 1, s + array[index], temp.concat(array[index]));
-			fork(index, s + array[index], temp.concat(array[index]));
+			fork(index, s + array[index], temp.concat(limit.indexOf(array[index])));
 		}
 		if (s + array[index] >= sum) {
-			console.log(4, s, array[index], sum);
-			result.push(temp.concat(array[index]));
+			// console.log(4, s, array[index], sum);
+			// console.log("4");
+			result.push(temp.concat(limit.indexOf(array[index])));
+			fork(index + 1, s, temp);
 			return;
 		}
 		// if (array[index] + array[index] >= sum) {
@@ -144,12 +188,15 @@ function getSubsets(array, sum) {
 		// 	result.push(temp.concat(array[index + 1]));
 		// 	return;
 		// }
-		console.log(6, index + 1, s, temp);
+		// console.log(6, index + 1, s, temp);
+		// console.log("5");
 		fork(index + 1, s, temp);
 	}
 	var result = [];
 	fork();
+	// console.log(result);
 	return result;
 }
 
-// console.log(getSubsets(limit, 35));
+// console.log(getSubsets(limit, 51));
+calculate();
--- src/main.ts
+++ src/main.ts
@@ -1,74 +1,235 @@
-const limit: number[] = [350, 330, 310, 290, 270, 250, 225, 200, 150, 50];
-const price: number[] = [500, 400, 300, 200, 100, 50, 20, 10, 5, 2];
-interface Option {
-	limits: number[];
-	quantity: number;
-	price: number;
-}
+/* eslint-disable @typescript-eslint/typedef */
+"use strict";
 
-const textInput: HTMLTextAreaElement = document.createElement("textarea");
-textInput.addEventListener("keyup", calculate);
+const limit = [350, 330, 310, 290, 270, 250, 225, 200, 150, 50];
+const prices = [500, 400, 300, 200, 100, 50, 20, 10, 5, 2];
 
-function calculate(event: KeyboardEvent) {
-	const textLength: number = textInput.value.length;
-	const options: Option[] = findOptions(textLength);
-}
+// https://stackoverflow.com/questions/53659151/return-all-subsets-whose-sum-is-a-given-value-subset-sum-problem
+document.addEventListener("DOMContentLoaded", function () {
+	const textInput = document.createElement("span");
+	textInput.setAttribute("class", "textarea");
+	textInput.setAttribute("id", "textinput");
+	textInput.setAttribute("maxlength", "1000");
+	textInput.setAttribute("contenteditable", "true");
+	textInput.innerText = "This is a sample superchat message! Lorem Ipsum... This will appear in a new $2 superchat.";
+	const textData = document.createElement("div");
+	textData.setAttribute("id", "textdata");
+	const superChatValues = document.getElementById("superchatvalues");
+	const superchattextbox = document.getElementById("superchattextbox");
+	const mainElement = document.getElementById("main");
+	textInput.addEventListener("keyup", calculate);
 
-function findOptions(length: number, index: number = 0) {
-	const results: Option[] = [];
-	for (index; index < limit.length; index++) {
-		let currentLength: number = length;
-		const result: Option = {
-			limits: [],
-			quantity: 0,
-			price: 0,
-		};
-		if (limit[index] < length) {
-			result.limits.push(limit[index]);
-			currentLength = currentLength - limit[index];
-			let localIndex: number = index;
-			for (localIndex; localIndex < limit.length; localIndex++) {
-				currentLength = recursive(result, localIndex, currentLength);
+	// superChatSelect.addEventListener("change", calculate);
+	for (const [key, value] of prices.entries()) {
+		const optionElement = document.createElement("input");
+		const superchatElement = document.createElement("div");
+		const spanElement = document.createElement("span");
+		const labelElement = document.createElement("label");
+		const inputElement = document.createElement("input");
+		labelElement.innerText = `$${value}`;
+		spanElement.appendChild(labelElement);
+		labelElement.appendChild(inputElement);
+		inputElement.setAttribute("id", `sc_${key}`);
+		superchatElement.setAttribute("data-superchat-id", `${key}`);
+		superchatElement.setAttribute("class", `${superchatTier(key)} superchat-card`);
+		spanElement.setAttribute("class", `contents`);
+		inputElement.setAttribute("value", `${value}`);
+		inputElement.setAttribute("type", "text");
+		optionElement.setAttribute("value", `${key}`);
+		optionElement.setAttribute("checked", "true");
+		optionElement.setAttribute("id", `sco_${key}`);
+		optionElement.setAttribute("type", "checkbox");
+		optionElement.addEventListener("change", calculate);
+		spanElement.appendChild(optionElement);
+		superchatElement.appendChild(spanElement);
+		if (superChatValues) {
+			superChatValues.appendChild(superchatElement);
+		}
+	}
+	if (superchattextbox && mainElement) {
+		superchattextbox.appendChild(textInput);
+		mainElement.appendChild(textData);
+	}
+});
+let timer = -1;
+
+function toggleSuperchatColors() {
+	const superChatValues = document.getElementById("superchatvalues");
+	if (superChatValues) {
+		for (let index = 0; index < superChatValues?.children.length; index++) {
+			const element = superChatValues?.children[index];
+			// console.log(
+			// 	element,
+			// 	element.children[0].children[1],
+			// 	superchatTier(element.dataset.superchatId),
+			// 	element.dataset.superchatId,
+			// );
+			const option: HTMLInputElement = element.children[0].children[1] as HTMLInputElement;
+			const superChatId: string | null = element.getAttribute("data-superchat-id");
+			let superChatTier: string = "";
+			if (typeof superChatId === "string") {
+				superChatTier = superchatTier(parseInt(superChatId, 10));
 			}
-		} else {
-			result.limits.push(limit[index]);
+			if (option.checked === false) {
+				element.classList.remove(superChatTier);
+				element.classList.add("none");
+			} else {
+				element.classList.add(superChatTier);
+				element.classList.remove("none");
+			}
 		}
-		//calculate price and quantity here
-		results.push(result);
 	}
-	return results;
 }
 
-function recursive(option: Option, index: number, length: number) {
-	let currentLength: number = length;
-	if (limit[index] < length) {
-		option.limits.push(limit[index]);
-		currentLength = currentLength - limit[index];
-		let localIndex: number = index;
-		for (localIndex; localIndex < limit.length; localIndex++) {
-			currentLength = recursive(option, localIndex, currentLength);
-		}
-	} else {
-		option.limits.push(limit[index]);
+function calculate() {
+	clearTimeout(timer);
+	toggleSuperchatColors();
+	const textInput = document.getElementById("textinput");
+	const textdata = document.getElementById("textdata");
+	if (textInput && textdata) {
+		timer = window.setTimeout(function () {
+			const values = getSelectedSuperchatOptions();
+			const textLength = textInput.innerText.length;
+			// console.log(textLength);
+			const options = getSubsets(values, textLength);
+			const fragment = document.createDocumentFragment();
+			for (const option of options) {
+				let remainingText = textInput.innerText;
+				let price = 0;
+				fragment.appendChild(document.createElement("hr"));
+				const superchatElement = document.createElement("div");
+				const superchatHeaderElement = document.createElement("div");
+				const superChatStatsElement = document.createElement("div");
+				superchatElement.setAttribute("class", `none superchat-card`);
+				superchatHeaderElement.setAttribute("class", `header`);
+				superChatStatsElement.setAttribute("class", `contents`);
+				superchatHeaderElement.innerText = `Stats`;
+				superchatElement.appendChild(superchatHeaderElement);
+				superchatElement.appendChild(superChatStatsElement);
+				fragment.appendChild(superchatElement);
+				for (const index of option) {
+					const characterLimit = limit[index];
+					const superchatvalue: HTMLInputElement = document.getElementById(`sc_${index}`) as HTMLInputElement;
+					if (superchatvalue) {
+						const dollarValue = superchatvalue.value;
+						const message = remainingText.match(new RegExp(`(.|[\r\n]){1,${characterLimit}}`, "g"));
+						if (message && message[0]) {
+							const superchatElement = document.createElement("div");
+							const superchatHeaderElement = document.createElement("div");
+							const preElement = document.createElement("div");
+							superchatElement.setAttribute("class", `${superchatTier(index)} superchat-card`);
+							superchatHeaderElement.setAttribute("class", `header`);
+							preElement.setAttribute("class", `contents`);
+							superchatHeaderElement.innerText = `$${dollarValue} \t (${message[0].length}/${limit[index]})`;
+							preElement.innerText = message[0];
+							superchatElement.appendChild(superchatHeaderElement);
+							superchatElement.appendChild(preElement);
+							fragment.appendChild(superchatElement);
+							message[0] = "";
+							remainingText = message.join("");
+							price += prices[index];
+						}
+					}
+				}
+				superChatStatsElement.innerText = `Cost: ${price}, Characters: ${textInput.innerText.length}`;
+			}
+			textdata.innerText = "";
+			textdata.appendChild(fragment);
+			// textData.innerText = textLength + "\n" + JSON.stringify(options, null, "\t");
+		}, 300);
 	}
-	return currentLength;
 }
 
-// function permute(nums) {
-// 	let result = [];
-// 	if (nums.length === 0) return [];
-// 	if (nums.length === 1) return [nums];
-// 	for (let i = 0; i < nums.length; i++) {
-// 		const currentNum = nums[i];
-// 		const remainingNums = nums.slice(0, i).concat(nums.slice(i + 1));
-// 		const remainingNumsPermuted = permute(remainingNums);
-// 		for (let j = 0; j < remainingNumsPermuted.length; j++) {
-// 			const permutedArray = [currentNum].concat(remainingNumsPermuted[j]);
-// 			result.push(permutedArray);
-// 		}
-// 	}
-// 	return result;
-// }
+function superchatTier(index: number | undefined) {
+	if (!index) {
+		return "none";
+	}
+	if (index < 5) {
+		return "aka";
+	}
+	if (index === 5) {
+		return "fifty";
+	}
+	if (index === 6) {
+		return "twenty";
+	}
+	if (index === 7) {
+		return "ten";
+	}
+	if (index === 8) {
+		return "five";
+	}
+	if (index === 9) {
+		return "two";
+	}
+	return "none";
+}
 
-// test code
-findOptions(500);
+function getSelectedSuperchatOptions() {
+	const values = [];
+	for (const [key] of prices.entries()) {
+		const superchatCheckbox: HTMLInputElement = document.getElementById(`sco_${key}`) as HTMLInputElement;
+		if (superchatCheckbox) {
+			if (superchatCheckbox.checked) {
+				values.push(limit[key]);
+			}
+		}
+	}
+	// console.log(values);
+	return values;
+}
+
+// add termination condition, if s > 0?
+
+function getSubsets(array: number[], sum: number) {
+	function fork(index = 0, s = 0, temp: number[] = []) {
+		if (s >= sum) {
+			// console.log(1, s, sum);
+			result.push(temp);
+			// console.log("1");
+			// fork(index + 1, s, temp);
+			return;
+		}
+		if (index === array.length) {
+			// console.log(2, index, array.length);
+			// console.log("2");
+			return;
+		}
+		if (s + array[index] <= sum) {
+			// shout circuit for positive numbers only
+			// console.log(3, s, array[index], sum);
+			// console.log("3");
+			// fork(index + 1, s + array[index], temp.concat(array[index]));
+			fork(index, s + array[index], temp.concat(limit.indexOf(array[index])));
+		}
+		if (s + array[index] >= sum) {
+			// console.log(4, s, array[index], sum);
+			// console.log("4");
+			result.push(temp.concat(limit.indexOf(array[index])));
+			fork(index + 1, s, temp);
+			return;
+		}
+		// if (array[index] + array[index] >= sum) {
+		// 	console.log(5, array[index], array[index], sum);
+		// 	result.push(temp.concat(array[index], array[index]));
+		// 	return;
+		// }
+		// if (s + array[index] >= sum) {
+		// 	// shout circuit for positive numbers only
+		// 	console.log(4, s, array[index], sum);
+
+		// 	result.push(temp.concat(array[index + 1]));
+		// 	return;
+		// }
+		// console.log(6, index + 1, s, temp);
+		// console.log("5");
+		fork(index + 1, s, temp);
+	}
+	const result: number[][] = [];
+	fork();
+	// console.log(result);
+	return result;
+}
+
+// console.log(getSubsets(limit, 51));
+calculate();
